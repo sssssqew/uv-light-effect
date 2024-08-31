6d010d75ea476f8c9b8bbe19477ae552a667f0b2
sssssqew <sssssqew@naver.com>
2024-08-31 15:22:49 +0900

    uv 광선 효과 적용하기

---
+++ index.js
@@ -0,0 +1,10 @@
+let pos = document.documentElement
+let uvLight = document.querySelector('.uv-light')
+console.log(pos)
+
+pos.style.setProperty('--x', '0px')
+
+document.body.addEventListener('mousemove', (e) => {
+    pos.style.setProperty('--x', e.clientX + 'px')
+    uvLight.style.left = (e.clientX - 15) + 'px'
+})
